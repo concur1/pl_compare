6eee6bf76e49781b744e65b92a0d7869969debb6
concur <4nks65tlu@mozmail.com>
2024-03-03 17:00:20 +0000

    Updated.

--- run.py
+++ run.py
@@ -24,3 +24,159 @@ def compare_df():
     )
 
 print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
+import polars as pl
+from polars.testing import assert_frame_equal
+from pl_compare.compare import compare
+
+
+def base_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+def compare_df():
+    return pl.DataFrame(
+        {
+            "ID": ["123456", "1234567", "1234567810"],
+            "Example1": [1, 2, 3],
+            "Example2": [1, 2, 3],
+            "Example3": [1, 2, 3],
+        },
+    )
+
+print(compare(["ID", "Example1"],base_df(), compare_df()).report())
