43756d4286b0034892e3c8ed8f9f491ababfce83
concur1 <ahcyaskx4@relay.firefox.com>
2026-02-27 16:40:09 +0000

    test value.

--- repro.py
+++ repro.py
@@ -4,12 +4,12 @@ print(f"Polars version: {pl.__version__}")
 
 lf = pl.LazyFrame({
     "id": [1, 2],
-    "variable": ["existing_data", "existing_data"] 
+    "value": ["existing_data", "existing_data"] 
 })
 
 result = lf.unpivot(
     index="id",
-    on=["variable"], # We are melting a column into a new column with the same name
+    on=["value"], # We are melting a column into a new column with the same name
     variable_name="variable",
     value_name="value"
 ).collect()
