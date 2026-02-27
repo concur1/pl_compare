1b793cb7ce6aca503fc32f607b3045baa530d7ee
concur1 <ahcyaskx4@relay.firefox.com>
2026-02-27 17:13:08 +0000

    versions

--- repro.py
+++ repro.py
@@ -1,18 +1,4 @@
 import polars as pl
 import sys
 
-print(f"Polars version: {pl.__version__}")
-
-lf = pl.LazyFrame({
-    "id": [1, 2],
-    "value": ["existing_data", "existing_data"] 
-})
-
-result = lf.unpivot(
-    index="id",
-    on=["value"], # We are melting a column into a new column with the same name
-    variable_name="exampe_variable",
-    value_name="example_value"
-).collect()
-
-print(result)
+print(pl.show_versions())
